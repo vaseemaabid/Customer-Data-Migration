5791489d9f89405a7d00828c6d9704aa52c1c4f8
vaseemaabid <154531298+vaseemaabid@users.noreply.github.com>
2026-06-22 15:41:49 +0530

    Implement data cleaning in clean_data.py
    Added data cleaning steps to remove duplicates and NaN values.

--- clean_data.py
+++ clean_data.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+df = pd.read_csv("customer_data.csv")
+
+print("Before Cleaning:")
+print(df)
+
+df = df.drop_duplicates()
+df = df.dropna()
+
+print("\nAfter Cleaning:")
+print(df)
